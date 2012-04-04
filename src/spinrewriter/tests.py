064562ef2aef32bfeb85ab9eff755b8cdf36b0bf
Nejc Zupan <nejc.zupan@gmail.com>
2012-04-04 17:33:02 +0200

    add test for Api.unique_variation()

--- src/spinrewriter/tests.py
+++ src/spinrewriter/tests.py
@@ -9,6 +9,10 @@ class TestApi(unittest.TestCase):
 
     @mock.patch('spinrewriter.urllib2')
     def test_api_quota(self, urllib2):
+        """Test if Api.api_quota() correctly parses the response it gets from
+        SpinRewriter API.
+        """
+
         # mock response from urllib2
         mocked_response = u"""{
             "status":"OK",
@@ -29,12 +33,16 @@ class TestApi(unittest.TestCase):
 
     @mock.patch('spinrewriter.urllib2')
     def text_with_spintax(self, urllib2):
+        """Test if Api.text_with_spintax() correctly parses the response it gets
+        from SpinRewriter API.
+        """
+
         # mock response from urllib2
         mocked_response = u"""{
             "status":"OK",
             "response":"This is my {little|limited|minimal} {pet|animal|dog|cat} {who|that} {read|checked out|reviewed|went over|went through|studied} {an article|a write-up|a post|a blog|a piece} {about|regarding|concerning|pertaining to|relating to} {animal|cat|pet} food.",
-            "api_requests_made":19,
-            "api_requests_available":81,
+            "api_requests_made":20,
+            "api_requests_available":80,
             "protected_terms":"food, cat",
             "nested_spintax":"false",
             "confidence_level":"medium"
@@ -50,9 +58,43 @@ class TestApi(unittest.TestCase):
 
         # test results
         self.assertEquals(result['status'], u'OK')
-        self.assertEquals(result['api_requests_made'], 19)
-        self.assertEquals(result['api_requests_available'], 81)
+        self.assertEquals(result['api_requests_made'], 20)
+        self.assertEquals(result['api_requests_available'], 80)
         self.assertEquals(result['protected_terms'], u"food, cat")
         self.assertEquals(result['nested_spintax'], u"false")
         self.assertEquals(result['confidence_level'], u"medium")
         self.assertEquals(result['response'], u"This is my {pet|animal|dog|cat} food.")
+
+    @mock.patch('spinrewriter.urllib2')
+    def unique_variation(self, urllib2):
+        """Test if Api.unique_variation() correctly parses the response it gets
+        from SpinRewriter API.
+        """
+
+        # mock response from urllib2
+        mocked_response = u"""{
+            "status":"OK",
+            "response":"This is my animal food.",
+            "api_requests_made":21,
+            "api_requests_available":79,
+            "protected_terms":"food, cat",
+            "nested_spintax":"false",
+            "confidence_level":"medium"
+        }"""
+        urllib2.urlopen.return_value.read.return_value = mocked_response
+
+        # call API
+        api = Api('foo@bar.com', 'my_api_key')
+        result = api.unique_variation(
+            text="This is my pet food.",
+            protected_terms=['food', 'cat']
+        )
+
+        # test results
+        self.assertEquals(result['status'], u'OK')
+        self.assertEquals(result['api_requests_made'], 21)
+        self.assertEquals(result['api_requests_available'], 79)
+        self.assertEquals(result['protected_terms'], u"food, cat")
+        self.assertEquals(result['nested_spintax'], u"false")
+        self.assertEquals(result['confidence_level'], u"medium")
+        self.assertEquals(result['response'], u"This is my animal food.")
