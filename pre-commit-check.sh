fc66eeb5925b007b7697abd5cc45c2ac65279530
Nejc Zupan <nejc.zupan@gmail.com>
2012-07-26 14:31:17 +0200

    a TODO entry

--- pre-commit-check.sh
+++ pre-commit-check.sh
@@ -13,6 +13,7 @@ bin/test; handle_exit
 
 echo '====== Syntax validation ======'
 bin/vvv setup.py; handle_exit
+# bin/vvv docs/; handle_exit # TODO: fix validator so that it works without Python 3
 bin/vvv src/spinrewriter; handle_exit
 
 if [ $EXITCODE -ne 0 ]; then
